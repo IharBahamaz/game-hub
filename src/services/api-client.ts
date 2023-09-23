3fc317c78b2bd8d16897255fb3796d533abdb4b6
IllusoryedoC <codeoffantasy@gmail.com>
2023-09-24 01:58:29 +0200

    Refactor-simplify game query + FetchingResponse

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,7 +2,7 @@ import axios, { AxiosRequestConfig } from 'axios';
 
 export interface FetchResponse<T> {
   count: number;
-  next: string | null;
+  next?: string | null;
   results: T[];
 }
 
@@ -20,9 +20,14 @@ class APIClient<T> {
     this.endpoint = endpoint;
   }
 
-  getAll = (config: AxiosRequestConfig) => {
-    return axiosInstance
+  getAll = async (config: AxiosRequestConfig) => {
+    return await axiosInstance
       .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+  get = async (id: number | string) => {
+    return await axiosInstance
+      .get<T>(this.endpoint + '/' + id)
       .then((res) => res.data);
   };
 }
